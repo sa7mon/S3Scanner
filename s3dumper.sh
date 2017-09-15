d834706e375c1bb261058c56a43ab20f95646ce0
Dan Salmon <salmon@protonmail.ch>
2017-09-14 21:00:48 -0500

    Cleanup output

--- s3dumper.sh
+++ s3dumper.sh
@@ -7,7 +7,7 @@
 # Author: Dan Salmon (twitter.com/bltjetpack, github.com/sa7mon)
 # Created: 6/20/2017
 #
-# Requirements: aws-cli [pip install awscli]
+# Requirements: aws-cli (pip install awscli)
 # 
 #############################################################################
 
@@ -25,10 +25,10 @@ s3file=$1
 while read line
 do
     # Do what you want to $domain
-    domain=`echo $line | awk -F ':' '{print $1}'`
-    region=`echo $line | awk -F ':' '{print $2}'`
+    domain=`echo $line | awk -F ':' '{print tolower($1)}'`
+    region=`echo $line | awk -F ':' '{print tolower($2)}'`
 
-    echo "[$domain] Checking..."
+    echo "[$domain] Checking domain: $domain:$region"
     awsls=`aws s3 ls s3://$domain --no-sign-request --region $region 2>&1`
 
     # Check if we get an "AccessDenied" error while attempting to 
@@ -39,15 +39,25 @@ do
   		echo "[$domain]    Nope. Access denied."
   	else
   		echo "[$domain]    Seems good!"
-  		# Create directory if it doesn't already exist
-  		if [ ! -d "./buckets/$domain" ]; then
-		  mkdir -p "./buckets/$domain";
-		fi
 
-		# Download bucket into directory
-		aws s3 sync s3://$domain ./buckets/$domain/ --no-sign-request --region $region
-	
-    # TODO: Here if the bucket/domain folder is empty, delete it
-  fi
+  		# Create directory if it doesn't already exist
+  		if [ ! -d "./buckets/$domain" ] 
+      then
+		    mkdir -p "./buckets/$domain";
+      fi
+      
+		  # Download bucket into directory
+		  aws s3 sync s3://$domain ./buckets/$domain/ --no-sign-request --region $region
+
+      # Check if dumped bucket folder is empty and delete if it is.       
+      target="./buckets/$domain"
+      if ! find "$target" -mindepth 1 -print -quit | grep -q .; then
+          echo "[$domain]    Folder is empty. Deleting..."
+          rmdir $target
+      fi
+    fi
+
+  
+  # fi
 done < $s3file
  
