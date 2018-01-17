18c7f521d15b4559522affcaff7062e526cfd9b3
Dan Salmon <salmon@protonmail.ch>
2018-01-16 19:33:42 -0600

    Switch to using GET requests instead of messy DNS lookups

--- s3finder.py
+++ s3finder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#########
 #
 # AWS S3scanner - Scans domain names for A records that resolve to *.amazonaws.com
 # 
@@ -8,57 +8,49 @@
 #
 #########
 
-import dns.resolver
-import socket
-import dns.reversename
-import subprocess
-import re
 import argparse
+import requests
 
-resolver = dns.resolver.Resolver()
-resolver.nameservers=['8.8.8.8', '8.8.4.4', '208.67.222.222', '208.67.220.220', '216.146.35.35', '216.146.36.36', socket.gethostbyname('ns1.cisco.com')]
-
-serverCountLimit = 2
-logFileName = "results.txt"
+defaultRegion = "us-west-1"
 
 
 def pprint(good, message):
     if good:
-		# print in green
-		print("\033[0;32m" + message + "\033[0;m")
-		logFile.write(message + "\n")
+        # print in green
+        print("\033[0;32m" + message + "\033[0;m")
+        logFile.write(message + "\n")
     else:
-    	# print in red
-    	print("\033[0;91m" + message + "\033[0;m")
+        # print in red
+        print("\033[0;91m" + message + "\033[0;m")
 
 
-def checkSite(site):
-	i = 1
-	try:
-		for rdata in resolver.query(site, 'A'):
-			if (i > serverCountLimit):
-				pprint(False, "Skipping " + site + " additional A server(s)...")
-				continue
-			resultText = "	A record: " + str(rdata) + " - "
+def checkSite(site, region):
+    """ Does a simple GET request with the Requests library and interprets the results.
 
-			nslookup = subprocess.Popen("nslookup " + str(rdata),stdout=subprocess.PIPE,shell=True)
-			match = re.search("(?:name = s3-website-)(.*)(?:\.amazonaws\.com)",nslookup.stdout.read())
-			if match:
-				aws = str(match.group(1))
-				printLine = site + ":" + aws
-				print("\033[0;32m" + printLine + "\033[0;m")
-				logFile.write(printLine + "\n")
-			else:
-				pprint(False, site + " : " + str(rdata) + " : Not")
-			i += 1
-	except dns.resolver.NoAnswer as err:
-		pprint(False, "Caught 'No A Record' error. Skipping...")
-	except dns.resolver.NXDOMAIN as err:
-		pprint(False, "Caught 'No NX Domain' error. Skipping...")
-	except dns.resolver.NoNameservers as err:
-		pprint(False, "Caught 'All nameservers failed' error. Skipping...")
-	except dns.exception.Timeout as err:
-		pprint(False, "Caught 'DNS timout' error. Skipping...")
+    site - A domain name without protocol (http[s])
+    region - An s3 region. See: https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+    """
+
+    # Concat domain name with the default region
+    bucketDomain = 'http://' + site + '.s3-' + region + '.amazonaws.com'
+    r = requests.get(bucketDomain)
+
+    if r.status_code == 200:
+        # Successfully found a bucket!
+        pprint(True, site + ":" +
+               region)
+    elif r.status_code == 301:
+        # We got the region wrong. The 'x-amz-bucket-region' header will give us the correct one.
+        checkSite(site, r.headers['x-amz-bucket-region'])
+    elif r.status_code == 403:
+        # We probably need to have an AWS account defined.
+        pprint(False, site + ":" + region + ' - Need an authenticated user to verify')
+    elif r.status_code == 404:
+        # This is definitely not a valid bucket name.
+        pprint(False, site)
+    else:
+        raise ValueError("Got an unhandled status code back: " + str(r.status_code) + " for site: " + site + ":" + region)
+
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Find S3 sites!')
@@ -70,13 +62,10 @@ parser.add_argument('domains', help='Name of text file containing domains to che
 # Parse the args
 args = parser.parse_args()
 
-
 # Open log file for writing
 logFile = open(args.outFile, 'a+')
 
 with open(args.domains, 'r') as f:
-	for line in f:
-		site = line.rstrip()
-		checkSite(site)
-
-
+    for line in f:
+        site = line.rstrip()
+        checkSite(site, defaultRegion)
