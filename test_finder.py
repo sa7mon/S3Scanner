3341cd4d79cadf0197b4614c42399b102cf5f63a
Dan Salmon <salmon@protonmail.ch>
2018-01-19 09:43:06 -0600

    Iron out testing issues

--- test_finder.py
+++ test_finder.py
@@ -7,17 +7,22 @@ def test_getBucketSize():
     Scenario 1: Bucket doesn't exist
         Expected: 255
 
-    Scenario 2: Bucket exists, region correct, listing denied
-
-    Scenario 3: Bucket exists, region correct, listing open to public
+    Scenario 2: Bucket exists, listing open to public
+        Expected:
+            Size: 9.1 KiB
+        Note:
+            Using flaws.cloud as example by permission of owner (@0xdabbad00)
 
     """
 
     # Scenario 1
     try:
-        s3.getBucketSize('example-this-hopefully-wont-exist-123123123')
+        result = s3.getBucketSize('example-this-hopefully-wont-exist-123123123')
     except sh.ErrorReturnCode_255:
         assert True
+
+    # Scenario 3
+    assert s3.getBucketSize('flaws.cloud') == "9.1 KiB"
 
 
 def test_checkBucket():
@@ -27,13 +32,18 @@ def test_checkBucket():
             Code: 301
             Region: Region returned depends on the closest S3 region to the user. Since we don't know this,
                     just assert for 2 hyphens.
-
         Note:
             Amazon should always give us a 301 to redirect to the nearest s3 endpoint.
-            Currently uses the ap-south-1 (Asia Pacific - Mumbai) region, so if you're running the test
-            near there, change to a region far from you like us-west-1.
+            Currently uses the ap-south-1 (Asia Pacific - Mumbai) region, so if you're running
+            the test near there, change to a region far from
+            you - https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
 
     Scenario 2: Bucket exists, region correct
+        Expected:
+            Code: 200
+            Message: Contains the domain name and region
+        Note:
+            Using flaws.cloud as example by permission of owner (@0xdabbad00)
 
     """
     # Scenario 1
@@ -42,3 +52,6 @@ def test_checkBucket():
     assert result[1].count("-") == 2
 
     # Scenario 2
+    result = s3.checkBucket('flaws.cloud', 'us-west-2')
+    assert result[0] == 200
+    assert 'flaws.cloud:us-west-2' in result[1]
