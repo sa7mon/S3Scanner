1d9c715f9a41cba085e712d7f8ddc31f674510fc
Dan Salmon <dan.sa7mon@gmail.com>
2020-01-16 20:51:14 -0600

    Set timeout to 1 hour

--- scripts/dev_create-danger1-bucket.sh
+++ scripts/dev_create-danger1-bucket.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Time to wait before deleting the dangerous bucket (in seconds)
-TIMEOUT=10
+# Time in seconds to wait before deleting the dangerous bucket (1 hour)
+TIMEOUT=3600
 
 # Generate a random alphanumeric (lower-case) 50 character string for a new bucket name
 BUCKET_NAME=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 50 | head -n 1)
@@ -11,8 +11,9 @@ aws s3api create-bucket --profile privileged --bucket "$BUCKET_NAME" --grant-wri
 --grant-write-acp uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers
 
 # Pause the script for x seconds
-echo "Bucket will be deleted in $TIMEOUT seconds. Press enter to delete now: "
+echo "Bucket will be deleted in $TIMEOUT seconds. Press <enter> to delete now: "
 read -t $TIMEOUT -r answer
 
+# Delete the bucket after the time expires or the user hit <enter>
 echo "Deleting bucket..."
 aws s3api delete-bucket --profile privileged --bucket "$BUCKET_NAME"
