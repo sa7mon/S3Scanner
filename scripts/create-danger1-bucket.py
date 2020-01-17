6b847d749b233c0406662b74d1cde36586f97954
Dan Salmon <dan.sa7mon@gmail.com>
2020-01-17 12:19:27 -0600

    Probably break script

--- scripts/create-danger1-bucket.py
+++ scripts/create-danger1-bucket.py
@@ -3,12 +3,18 @@ import string
 from time import sleep
 import boto3
 
-TIMEOUT = 3600
+TIMEOUT = 15
 
 
 def generate_random_bucket_name(length=50):
     candidates = string.ascii_lowercase + string.digits
     return ''.join(random.choice(candidates) for i in range(length))
+
+
+def delete_bucket():
+    global bucket_name
+    global s3_client
+    s3_client.delete_bucket(Bucket=bucket_name)
 
 
 bucket_name = generate_random_bucket_name(50)
@@ -20,14 +26,8 @@ print("Creating bucket: " + bucket_name)
 # Create bucket
 s3_client.create_bucket(Bucket=bucket_name, GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers',
                         GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
+export 
 
 print("Waiting " + str(TIMEOUT) + " seconds to delete the bucket. Hit Ctrl-C to delete now:")
-
-try:
-    for i in range(0, TIMEOUT):
-        sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    print("Deleting bucket...")
-    s3_client.delete_bucket(Bucket=bucket_name)
+sleep(TIMEOUT)
+delete_bucket()
