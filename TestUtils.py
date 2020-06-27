d7827b74230323fb15e3bab18258ec9ef63eeee4
Dan Salmon <dan.sa7mon@gmail.com>
2020-06-27 18:10:14 -0500

    Lint

--- TestUtils.py
+++ TestUtils.py
@@ -2,6 +2,7 @@ import random
 import string
 import boto3
 
+
 class TestBucketService:
     def __init__(self):
         self.session = boto3.Session(profile_name='privileged')
@@ -20,18 +21,28 @@ class TestBucketService:
 
         # For type descriptions, refer to: https://github.com/sa7mon/S3Scanner/wiki/Test-Buckets
         if danger_bucket == 1:
-            self.s3_client.create_bucket(Bucket=bucket_name, GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
-            self.s3_client.put_bucket_acl(Bucket=bucket_name, GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers', GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
+            self.s3_client.create_bucket(Bucket=bucket_name,
+                                         GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
+            self.s3_client.put_bucket_acl(Bucket=bucket_name,
+                                          GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers',
+                                          GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
         elif danger_bucket == 2:
-            self.s3_client.create_bucket(Bucket=bucket_name, GrantWrite='uri=http://acs.amazonaws.com/groups/global/AllUsers', GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AllUsers')
+            self.s3_client.create_bucket(Bucket=bucket_name,
+                                         GrantWrite='uri=http://acs.amazonaws.com/groups/global/AllUsers',
+                                         GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AllUsers')
         elif danger_bucket == 3:
-            self.s3_client.create_bucket(Bucket=bucket_name, GrantRead='uri=http://acs.amazonaws.com/groups/global/AllUsers', GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers', GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
+            self.s3_client.create_bucket(Bucket=bucket_name,
+                                         GrantRead='uri=http://acs.amazonaws.com/groups/global/AllUsers',
+                                         GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers',
+                                         GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers')
         elif danger_bucket == 4:
             self.s3_client.create_bucket(Bucket=bucket_name,
-                                         GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers,uri=http://acs.amazonaws.com/groups/global/AllUsers')
+                                         GrantWrite='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers,'
+                                                    'uri=http://acs.amazonaws.com/groups/global/AllUsers')
         elif danger_bucket == 5:
             self.s3_client.create_bucket(Bucket=bucket_name,
-                                         GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers,uri=http://acs.amazonaws.com/groups/global/AllUsers')
+                                         GrantWriteACP='uri=http://acs.amazonaws.com/groups/global/AuthenticatedUsers,'
+                                                       'uri=http://acs.amazonaws.com/groups/global/AllUsers')
         else:
             raise Exception("Unknown danger bucket type")
 
