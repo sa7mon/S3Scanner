536639d551f108a865e7a80ef8cc2457eef80a8b
Dan Salmon <dan@salmon.cat>
2025-08-17 22:02:51 -0500

    fix test and region check

--- cmd/regioncheck/digitalocean.go
+++ cmd/regioncheck/digitalocean.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"net/http"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 // GetRegionsDO fetches regions from the DigitalOcean docs HTML page.
@@ -25,12 +26,12 @@ func GetRegionsDO() ([]string, error) {
 	}
 
 	regions := []string{}
-	doc.Find("h2#other-digitalocean-products + table thead tr th").Each(func(_ int, t *goquery.Selection) {
+	doc.Find("h2#other-digitalocean-products + div table thead tr th").Each(func(_ int, t *goquery.Selection) {
 		regions = append(regions, t.Text())
 	})
 
 	spacesSupported := []bool{}
-	doc.Find("h2#other-digitalocean-products + table tbody tr").Each(func(_ int, t *goquery.Selection) {
+	doc.Find("h2#other-digitalocean-products + div table tbody tr").Each(func(_ int, t *goquery.Selection) {
 		// For each row, check the first cell for a value of "Spaces"
 		rowHeader := t.Find("td").First().Text()
 		if rowHeader == "Spaces" {
