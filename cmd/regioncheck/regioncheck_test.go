a9b299b90da54c439667699f637cc1949c603183
Dan Salmon <sa7mon@users.noreply.github.com>
2025-08-18 11:07:54 -0500

    fix: provider region checks (#417)
    * fix test and region check
    
    * fix digitalocean tests
    
    * fix linode tests
    
    * fix scaleway test
    
    * bump golangci go version
    
    * bump golangci-lint version and workflow version

--- cmd/regioncheck/regioncheck_test.go
+++ cmd/regioncheck/regioncheck_test.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRegionsDO(t *testing.T) {
 	r, err := GetRegionsDO()
 	assert.Nil(t, err)
-	assert.GreaterOrEqual(t, len(r), 1)
+	assert.Equal(t, 11, len(r))
 	assert.Contains(t, r, "nyc3")
 }
 
 func TestGetRegionsLinode(t *testing.T) {
 	r, err := GetRegionsLinode()
 	assert.Nil(t, err)
-	assert.Equal(t, len(r), 25)
+	assert.GreaterOrEqual(t, len(r), 28)
 	assert.Contains(t, r, "us-east-1")
 }
 
 func TestGetRegionsScaleway(t *testing.T) {
 	r, err := GetRegionsScaleway()
 	assert.Nil(t, err)
-	assert.GreaterOrEqual(t, len(r), 1)
+	assert.GreaterOrEqual(t, len(r), 3)
 	assert.Contains(t, r, "fr-par")
 }
